a4c94cf4a637342dd6881d27be43f91367ff677d
Evelin Gonzalez Feliu <37847170+evelingonzalezfeliu@users.noreply.github.com>
2022-11-04 15:40:18 -0300

    Add files via upload

--- run_somalier.sh
+++ run_somalier.sh
@@ -22,58 +22,6 @@ MAPPING="1_mapping"
 SOMALIER_EXTRACT="2_somalier_extract"
 SOMALIER_RELATE="3_somalier_relate"
 
-abort()
-{
-    echo >&3 '
-***************
-*** ABORTED ***
-***************
-'
-    echo "An error occurred. Exiting..." >&3
-    exit 1
-}
-trap 'abort' 0
-#abort on error
-set -e
-#set -o errexit
-
-### PARAMETROS DE ENTRADAS A TRAVES DE LINEA DE COMANDOS
-PARAMS=""
-#echo "1:$#"
-while (( "$#" )); do
-  case "$1" in
-    -i|--input--dir)
-      shift&&INDIR=$1
-      #echo "1:$#"
-      ;;
-    -o|--output--dir)
-      shift&&OUTDIR=$1
-      #echo "1:$#"
-      ;;
-    -t|--threads)
-       shift&&THREAD=$1
-       #echo "3:$#"
-      ;;
-    -s|--step)
-       shift&&STEP=$1
-       #echo "5:$#"
-      ;;
-    --) # end argument parsing
-      shift
-      break
-      ;;
-    -*|--*=) # unsupported flags
-      echo "Error: Unsupported flag $1" >&3
-      exit 1
-      ;;
-    *) # preserve positional arguments
-      PARAMS="$PARAMS $1"
-      shift
-      ;;
-  esac
-done
-# set positional arguments in their proper place
-eval set -- "$PARAMS"
 
 if [ -z "$OUTDIR" ]; then
 	echo ""
@@ -102,6 +50,10 @@ if [ -z "$THREAD" ]; then
 	read THREAD
 fi
 
+#echo $THREAD
+#echo $STEP
+#echo $INDIR
+#echo $OUTDIR
 ## agregar log por paso. (funcion), cambiar entrada de pasos. agregar comandos al log. poner tiempo por paso ejecutado.
 echo ""
 echo "############################################"
@@ -111,7 +63,7 @@ echo ""
 echo "== Extract informative sites, evaluate relatedness, and perform quality-control  =="
 
 #set -xv
-#trap read debug  
+#trap read debu
 
 #### función de creación de log por cada paso del pipeline
 #0 - stdin
@@ -132,21 +84,20 @@ step(){
 	if [ -z "$END" ]; then
 		END=$START
 	fi
-	
-	if (( $START > $END )); then
+
+	if [ $START -gt $END ]; then
 		echo "Error: The ${START} must by less than ${END} in step parameter ${STEP}" >&3
-		exit
+	exit
 	fi
 }
 
 start_log(){
 	n_step=$1
 	string_step=$2
-	
-	if [ ! -d "${OUTDIR}/${LOG}" ]; then 
+	if [ ! -d "${OUTDIR}/${LOG}" ]; then
 		mkdir "${OUTDIR}/${LOG}"
 	fi
-	
+
 	log_output="${OUTDIR}/${LOG}/${n_step}_log_${string_step}.out"
 	echo "$(date) : step ${n_step} - start - ${string_step}" >&3
 	echo "$(date) : step ${n_step} - logfile - ${log_output}" >&3
@@ -174,55 +125,57 @@ check_log(){
            exit
     fi
 	check=$(grep "DONE-somalier" $log_output | wc -l)
-	
+
 	if (($check == 0)); then
 		echo "$(date): ## Error ## - Step ${n_step} ${string_step} with error, it don't finished." >&3
 		echo "$(date): ## Error ## - Check log : ${log_output}"
 		exit
-	fi 
+	fi
 }
 
 get_samples(){
 	cd "${INDIR}"
 	echo $PWD
 	#GET SAMPLES ID FROM FASTQ FILES
-	SAMPLES=$(ls *R1_*.fastq.gz| awk '{split($0,array,"_")} {print array[1]}')
+	SAMPLES=$(ls *R1*.fastq.gz| awk '{split($0,array,"_")} {print array[1]}')
 }
 
 step $STEP
+echo $START
+echo $END
 
 #################################
-#								#
+#				#
 #    MAPPING OF READS (BWA)     #
 #################################
 
-if (( $START <= 1 )) && (( $END >= 1 )); then
+if [ $START -le 1 ] && [ $END -ge 1 ]; then
 
 start_log 1 "mapping"
 get_samples
 
-	if [ ! -d "${INDIR}/${MAPPING}" ]; then
-                mkdir "${INDIR}/${MAPPING}"
+	if [ ! -d "${OUTDIR}/${MAPPING}" ]; then
+                mkdir "${OUTDIR}/${MAPPING}"
         else
-                echo "The ${INDIR}/${MAPPING} directory alredy exists"
+                echo "The ${OUTDIR}/${MAPPING} directory alredy exists"
     fi
-    
+
 	for sample in $SAMPLES
-	do	
+	do
 		echo "Running bwa for $sample"
-        mapping_sam="${OUTDIR}/${MAPPING}/${sample}.sam"
+        	mapping_sam="${OUTDIR}/${MAPPING}/${sample}.sam"
   		mapping_bam="${OUTDIR}/${MAPPING}/${sample}.bam"
 		fastq_R1="${INDIR}/${sample}_R1.fastq.gz"
 		fastq_R2="${INDIR}/${sample}_R2.fastq.gz"
 		sorted_bam="${OUTDIR}/${MAPPING}/${sample}.sorted.bam"
-		
+
 		echo "time nice -n 11  bwa mem \
 		-t $THREAD \
 		-R \"@RG\tID:${sample}\tSM:${sample}\tPL:Illumina\tPU:unit1\tLB:lib1\" \
 		$hg19_fa \
 		$fastq_R1 \
 		$fastq_R2 > $mapping_sam"
-		
+
 		#work in parallel, quitar el echo
 		time nice -n 11  bwa mem \
 		-t $THREAD \
@@ -230,74 +183,76 @@ get_samples
 		$hg19_fa \
 		$fastq_R1 \
 		$fastq_R2 > $mapping_sam
-		           
+
 		#work in parallel
-        samtools view -@ $THREAD -bS $mapping_sam > $mapping_bam
+        	samtools view -@ $THREAD -bS $mapping_sam > $mapping_bam
 		samtools sort -@ $THREAD -o $sorted_bam -O BAM $mapping_bam
 		#don't work in parallel
 		samtools index $sorted_bam
 		rm $mapping_sam
 		rm $mapping_bam
 	done
-	
 end_log 1 "mapping"
 
 fi
 
 #################################
-#								#
+#				#
 #  EXTRACT SITIES (SOMALIER)    #
 #################################
 
-if (( $START <= 2 )) && (( $END >= 2 )); then
+if [ $START -le 2 ] && [ $END -ge 2 ]; then
 
 check_log 1 "mapping"
 start_log 2 "somalier-extract"
 get_samples
 
+	if [ ! -d "${OUTDIR}/${SOMALIER_EXTRACT}" ]; then
+                mkdir "${OUTDIR}/${SOMALIER_EXTRACT}"
+        else
+                echo "The ${OUTDIR}/${SOMALIER_EXTRACT} directory alredy exists"
+   	fi
+
         for sample in $SAMPLES
-	    do
-	    DIR_SOM="${OUTDIR}/${SOMALIER_EXTRACT}"
-	    echo "Running somalier extract for $sample"
-        somalier extract \
+	do
+
+	DIR_SOM="${OUTDIR}/${SOMALIER_EXTRACT}"
+	echo "Running somalier extract for $sample"
+	sorted_bam="${OUTDIR}/${MAPPING}/${sample}.sorted.bam"
+
+	somalier extract \
         -d $DIR_SOM \
         --sites $sites_hg19 \
-        -f $hg19_fa
+        -f $hg19_fa $sorted_bam
         done
 
 end_log 2 "somalier-extract"
 
 fi
 
-#################################
-#								#
-#  EXTRACT SITIES (SOMALIER)    #
-#################################
+####################################
+#				   #
+# CALCULATE - RELATE (SOMALIER)    #
+####################################
 
-if (( $START <= 3 )) && (( $END >= 3 )); then
+if [ $START -le 3 ] && [ $END -ge 3 ]; then
 
 check_log 2 "somalier-extract"
-start_log 3 "somalier-relate" 
+start_log 3 "somalier-relate"
+
+ 	if [ ! -d "${OUTDIR}/${SOMALIER_RELATE}" ]; then
+                mkdir "${OUTDIR}/${SOMALIER_RELATE}"
+        else
+                echo "The ${OUTDIR}/${SOMALIER_RELATE} directory alredy exists"
+        fi
 
 
 	    DIR_SOM="${OUTDIR}/${SOMALIER_EXTRACT}/*.somalier"
-	    DIR_RESULTS="${OUTDIR}/${SOMALIER_RELATE}"
+	    cd "${OUTDIR}/${SOMALIER_RELATE}"
 	    echo "Running somalier relate"
-	    somalier relate \
-	    --infer -i $DIR_SOM \
-	    -d $DIR_RESULTS
+
+	    somalier relate  --infer -i $DIR_SOM
 
 end_log 3 "somalier-relate"
 
 fi
-
-
-trap : 0
-
-echo >&3 '
-*********************
-*** DONE-somalier *** 
-*********************
-'
-
-
